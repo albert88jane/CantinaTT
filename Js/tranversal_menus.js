2c295ff52b73e3d49d1b8af3a86c3d16356b485d
elman <a19alvsildur@inspedralbes.cat>
2020-10-27 12:16:45 +0100

    Cambio de nombre de carpeta y cambio de formato de fichero menu

---
+++ Js/tranversal_menus.js
@@ -0,0 +1,21 @@
+<script  language="javascript" type="text/javascript">
+     window.onload= function(){
+
+         var horas = new Date();
+         var horaActual = horas.getHours();
+         var minutoActual = horas.getMinutes();
+         
+         if (horaActual <= 11 & minutoActual <= 30){
+
+             document.getElementById('menu1').style.display = "block";
+             document.getElementById('menu2').style.display = "none";
+         }
+
+         else{
+
+             document.getElementById('menu1').style.display = "none";
+             document.getElementById('menu2').style.display = "block";
+         }
+     }  
+</script>
+        
