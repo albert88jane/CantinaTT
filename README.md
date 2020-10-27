f22639250c5c30d3dce2af4e9b719d7a5d43111d
Albert <a19albjanmed@inspedralbes.cat>
2020-10-27 11:57:02 +0100

    Cambios en la cookie, funcion creada

--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Trevall Transversal Cantina
 
 ## Integrants Grup 6:
 
-	*Álvaro Silva Durant
-	*Bayron López Mejía
-	*Andrea Rondón Fuentes
-	*Albert Jané Medràn
+	* Álvaro Silva Durant
+	* Bayron López Mejía
+	* Andrea Rondón Fuentes
+	* Albert Jané Medràn
 
 
--- coockieCrear.php
+++ coockieCrear.php
@@ -3,14 +3,18 @@
 
 
 
-function(){
-    $cookie_name = "comprobacio";
-    $cookie_value = "albert";
-    $cookie_expire = mktime(date("j")) + 1;
+    function crearcookie($nombre_usuario){
+        
+        $cookie_name = "comprobacio";
+        $cookie_value = $nombre_usuario;
+        $cookie_expire = mktime(date("j")) + 1;
 
-setcookie($cookie_name, $cookie_value,$cookie_expire);
-}
+        setcookie($cookie_name, $cookie_value,$cookie_expire);
 
+    }
 
 
 ?>
+
+
+
