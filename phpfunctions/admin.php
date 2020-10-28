2b8248e56981398489bba5b90bf6669c8986e57e
Albert <a19albjanmed@inspedralbes.cat>
2020-10-28 13:19:28 +0100

    afegint fitxers per al login admin

---
+++ phpfunctions/admin.php
@@ -0,0 +1,36 @@
+
+ <?php
+
+
+
+
+    $logins = array("Albert"=>"albertpassword", "Alvaro"=>"alvaropassword", "Andrea"=>"andreapassword" , "Bayron"=>"bayronpassword");
+
+    $nom = $_REQUEST['usuari'];
+    $password = $_REQUEST['contrasenya'];
+    
+    
+   
+
+   if($password == $logins[$nom]){
+
+        session_start();
+        $_SESSION["admin"]=$nom;
+        header('Location: '. 'admin.php');
+        
+   }
+   else{
+
+        echo 'ERROR, CONTRASEÑA INCORRECTA';
+        include 'login.php';
+
+        
+   }
+
+   
+?>
+
+
+
+
+
---
+++ phpfunctions/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+
+</head>
+<body>
+    
+
+
+    <form action="admin.php" method="post">
+    
+    <input type="text" value="Nom Usuari" name="usuari"></input>
+    <input type="password" value= "Contrasenya" name="contrasenya"> </input>
+    <input type="submit" value="Log In"></input>
+
+
+
+    </form>
+
+
+   
+   
+
+</body>
+</html>
