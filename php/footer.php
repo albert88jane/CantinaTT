3c6470775c94e6d5761007016591d1b9a1f67b14
Bayron <a19baylopmej@inspedralbes.cat>
2020-10-29 10:06:13 +0100

    subir html

---
+++ php/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    
+        <div class="footer1">
+           <h1>Redes Sociales</h1>
+            <div class="row">
+                <img src="">
+                <label>Siguenos en Twitter</label>
+            </div>        
+        </div>
+        
+    </footer>
---
+++ php/header.php
@@ -0,0 +1,14 @@
+<header>  
+        <div class="barranav"> 
+            <div class="logo"><img src="img/logo1.png" width="130px">
+            </div>
+            <div class="titulo"><p>CANTINA PEDRALBES</p></div>
+            <div class="navegacion">
+                <nav>   
+                    <a href="index.html">INICIO</a>
+                    <a href="#menu.html">MENÚ</a>
+                    <a href="#comprar.html">COMPRAR</a> 
+                </nav>
+            </div>     
+        </div>
+    </header> 
