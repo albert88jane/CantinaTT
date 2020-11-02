ddf018f73df5f24c1983dcd13f9daefe43f35af5
AndreaPlatino <a19andronfue@inspedralbes.cat>
2020-11-02 13:43:54 +0100

    Afegeixo menu html (en extensió php), css menu i imatges emprades per la pàgina dels menus. (Fons i altres imatges i colors no tenen perqué ser les finals, queda pendent a valorar per la resta de l'equip).

---
+++ menu.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CANTINA PEDRALBES</title>
+    <link rel="stylesheet" href="/css/menu.css">
+    <script src="script.js" defer></script>
+</head>
+<body>
+    <?php include 'php/header.php';?>
+    <h1>Esmorzars i Berenars</h1>
+    <div class="menu1">
+        <div class="tipusMenjar1">
+            <h2>Dolç</h2>
+            <div class="productes">
+                <div class="producte1">
+                    <img class="imatgeProductes" src="./imatges/donutsxoco.jpg" alt="Donut">
+                    <div class="titol"><h3>Donut</h3></div>
+                    <div class="descripcio"><p>Donut de sucre o xocolata</p></div>
+                    <div class="preu"><h3>1,00 €</h3></div>
+                    <div>
+                        <form class="quantitat">
+                            <label for="fname">Quantitat:</label>
+                            <input type="number" name="fname" class="quantitat-boto" value="" placeholder="Màxim 4 unitats" maxlength="4">
+                        </form>
+                    </div>
+                </div>
+                <div class="producte2">
+                    <img class="imatgeProductes" src="./imatges/croissants.jpg" alt="Croissants">
+                    <div class="titol"><h3>Croissant</h3></div>
+                    <div class="descripcio"><p>Croissant normal o de xocolata</p></div>
+                    <div class="preu"><h3>1,20 €</h3></div>
+                    <div>
+                        <form class="quantitat">
+                            <label for="fname">Quantitat:</label>
+                            <input type="number" name="fname" class="quantitat-boto" value="" placeholder="Màxim 4 unitats" maxlength="4">
+                        </form>
+                    </div>
+                </div>
+                <div class="producte3">
+                    <img class="imatgeProductes" src="./imatges/canyesxoco.jpg" alt="Pastes de full">
+                    <div class="titol"><h3>Canyes</h3></div>
+                    <div class="descripcio"><p>Canya de crema o xocolata</p></div>
+                    <div class="preu"><h3>1,40 €</h3></div>
+                    <div>
+                        <form class="quantitat">
+                            <label for="fname">Quantitat:</label>
+                            <input type="number" name="fname" class="quantitat-boto" value="" placeholder="Màxim 4 unitats" maxlength="4">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div> 
+        <div class="tipusMenjar2">
+            <h2>Salat</h2>
+            <div class="productes">
+                <div class="producte1">
+                    <img class="imatgeProductes" src="./imatges/bikini.jpg" alt="Bikini">
+                    <div class="titol"><h3>Bikini</h3></div>
+                    <div class="descripcio"><p>De pernil dolç amb formatge o de pernil salat amb formatge</p></div>
+                    <div class="preu"><h3>1,60 €</h3></div>
+                    <div>
+                        <form class="quantitat">
+                            <label for="fname">Quantitat:</label>
+                            <input type="number" name="fname" class="quantitat-boto" value="" placeholder="Màxim 4 unitats" maxlength="4">
+                        </form>
+                    </div>
+                </div>
+                <div class="producte2">
+                    <img class="imatgeProductes" src="./imatges/bocatabutifarra.jpg" alt="Bocata de butifarra">
+                    <div class="titol"><h3>Entrepà calent</h3></div>
+                    <div class="descripcio"><p>Entrepà de llom, botifarra, bacon, hamburguesa o truita</p></div>
+                    <div class="preu"><h3>1,80 €</h3></div>
+                    <div>
+                        <form class="quantitat">
+                            <label for="fname">Quantitat:</label>
+                            <input type="number" name="fname" class="quantitat-boto" value="" placeholder="Màxim 4 unitats" maxlength="4">
+                        </form>
+                    </div>
+                </div>
+                <div class="producte3">
+                    <img class="imatgeProductes" src="./imatges/pizzaslice.jpg">
+                    <div class="titol"><h3>Pizza</h3></div>
+                    <div class="descripcio"><p>Porció de pizza a escollir</p></div>
+                    <div class="preu"><h3>1,40 €</h3></div>
+                    <div>
+                        <form class="quantitat">
+                            <label for="fname">Quantitat:</label>
+                            <input type="number" name="fname" class="quantitat-boto" value="" placeholder="Màxim 4 unitats" maxlength="4"> 
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div> 
+        <div class="tipusMenjar3">
+            <h2>Begudes</h2>
+            <div class="productes">
+                <div class="producte1">
+                    <img class="imatgeProductes" src="./imatges/xocolatadesfeta.jpg" alt="Xocolata desfeta">
+                    <div class="titol"><h3>Xocolata</h3></div>
+                    <div class="descripcio"><p>Xocolata desfeta</p></div>
+                    <div class="preu"><h3>1,40 €</h3></div>
+                    <div>
+                        <form class="quantitat">
+                            <label for="fname">Quantitat:</label>
+                            <input type="number" name="fname" class="quantitat-boto" value="" placeholder="Màxim 4 unitats" maxlength="4">
+                        </form>
+                    </div>
+                </div>
+                <div class="producte2">
+                    <img class="imatgeProductes" src="./imatges/cafeghost.jpg" alt="Cafè">
+                    <div class="titol"><h3>Cafè</h3></div>
+                    <div class="descripcio"><p>Cafè amb llet a escollir</p></div>
+                    <div class="preu"><h3>1,20 €</h3></div>
+                    <div>
+                        <form class="quantitat">
+                            <label for="fname">Quantitat:</label>
+                            <input type="number" name="fname" class="quantitat-boto" value="" placeholder="Màxim 4 unitats" maxlength="4">
+                        </form>
+                    </div>
+                </div>
+                <div class="producte3">
+                    <img class="imatgeProductes" src="./imatges/suctaronja.jpg" alt="Suc">
+                    <div class="titol"><h3>Suc</h3></div>
+                    <div class="descripcio"><p>Suc de taronja, prèssec o pinya</p></div>
+                    <div class="preu"><h3>1,40 €</h3></div>
+                    <div>
+                        <form class="quantitat">
+                            <label for="fname">Quantitat:</label>
+                            <input type="number" name="fname" class="quantitat-boto" value="" placeholder="Màxim 4 unitats" maxlength="4">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="demanar">
+        <form action="" method="POST"><input class="demanar-boto" type="submit" value="Enviar comanda"></form>
+    </div>
+    <h1>Dinars</h1>
+    <div class="menu2">
+        <div class="tipusMenjar1">
+            <h2>Entrepans</h2>
+            <div class="productes">
+                <div class="producte1">
+                    <img class="imatgeProductes" src="./imatges/sandwich.jpg" alt="Sandwich">
+                    <div class="titol"><h3>Sandwich Cantina</h3></div>
+                    <div class="descripcio"><p>Amb llom, ceba, formatge, tomàquet i amanida</p></div>
+                    <div class="preu"><h3>2,00 €</h3></div>
+                    <div>
+                        <form class="quantitat">
+                            <label for="fname">Quantitat:</label>
+                            <input type="number" name="fname" class="quantitat-boto" value="" placeholder="Màxim 4 unitats" maxlength="4">
+                        </form>
+                    </div>
+                </div>
+                <div class="producte2">
+                    <img class="imatgeProductes" src="./imatges/bocatabutifarra.jpg" alt="Entrepà calent">
+                    <div class="titol"><h3>Entrepà calent</h3></div>
+                    <div class="descripcio"><p>Entrepà de llom, botifarra, bacon, hamburguesa o truita</p></div>
+                    <div class="preu"><h3>1,80 €</h3></div>
+                    <div>
+                        <form class="quantitat">
+                            <label for="fname">Quantitat:</label>
+                            <input type="number" name="fname" class="quantitat-boto" value="" placeholder="Màxim 4 unitats" maxlength="4">
+                        </form>
+                    </div>
+                </div>
+                <div class="producte3">
+                    <img class="imatgeProductes" src="./imatges/bocatafred.jpg" alt="Entrepà fred">
+                    <div class="titol"><h3>Entrepà fred</h3></div>
+                    <div class="descripcio"><p>Embotit a escollir amb formatge, amanida i tomàquet</p></div>
+                    <div class="preu"><h3>1,90 €</h3></div>
+                    <div>
+                        <form class="quantitat">
+                            <label for="fname">Quantitat:</label>
+                            <input type="number" name="fname" class="quantitat-boto" value="" placeholder="Màxim 4 unitats" maxlength="4">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div> 
+        <div class="tipusMenjar2">
+            <h2>Plats Combinats</h2>
+            <div class="productes">
+                <div class="producte1">
+                    <img class="imatgeProductes" src="./imatges/burgueripatates.jpg" alt="Hamburguesa">
+                    <div class="titol"><h3>Hamburguesa</h3></div>
+                    <div class="descripcio"><p>Acompanyada de patates o amanida</p></div>
+                    <div class="preu"><h3>3,90 €</h3></div>
+                    <div>
+                        <form class="quantitat">
+                            <label for="fname">Quantitat:</label>
+                            <input type="number" name="fname" class="quantitat-boto" value="" placeholder="Màxim 4 unitats" maxlength="4">
+                        </form>
+                    </div>
+                </div>
+                <div class="producte2">
+                    <img class="imatgeProductes" src="./imatges/pastadeldia.jpg" alt="Pasta">
+                    <div class="titol"><h3>Pasta del dia</h3></div>
+                    <div class="descripcio"><p>Amb formatge i salsa de tomàquet o salsa carbonara</p></div>
+                    <div class="preu"><h3>5,50 €</h3></div>
+                    <div>
+                        <form class="quantitat">
+                            <label for="fname">Quantitat:</label>
+                            <input type="number" name="fname" class="quantitat-boto" value="" placeholder="Màxim 4 unitats" maxlength="4">
+                        </form>
+                    </div>
+                </div>
+                <div class="producte3">
+                    <img class="imatgeProductes" src="./imatges/peixiverdures.jpg">
+                    <div class="titol"><h3>Peix amb verdures</h3></div>
+                    <div class="descripcio"><p>Peix del dia amb verdures</p></div>
+                    <div class="preu"><h3>6,80 €</h3></div>
+                    <div>
+                        <form class="quantitat">
+                            <label for="fname">Quantitat:</label>
+                            <input type="number" name="fname" class="quantitat-boto" value="" placeholder="Màxim 4 unitats" maxlength="4"> 
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div> 
+        <div class="tipusMenjar3">
+            <h2>Begudes</h2>
+            <div class="productes">
+                <div class="producte1">
+                    <img class="imatgeProductes" src="./imatges/aigua.jpg" alt="Aigua">
+                    <div class="titol"><h3>Aigua</h3></div>
+                    <div class="descripcio"><p>Freda o natural de 0,75cl</p></div>
+                    <div class="preu"><h3>1,00 €</h3></div>
+                    <div>
+                        <form class="quantitat">
+                            <label for="fname">Quantitat:</label>
+                            <input type="number" name="fname" class="quantitat-boto" value="" placeholder="Màxim 4 unitats" maxlength="4">
+                        </form>
+                    </div>
+                </div>
+                <div class="producte2">
+                    <img class="imatgeProductes" src="./imatges/cocacola.jpg" alt="Gaseosa">
+                    <div class="titol"><h3>Gaseosa</h3></div>
+                    <div class="descripcio"><p>A escollir entre Fanta de llimona, fanta de taronja o CocaCola</p></div>
+                    <div class="preu"><h3>1,20 €</h3></div>
+                    <div>
+                        <form class="quantitat">
+                            <label for="fname">Quantitat:</label>
+                            <input type="number" name="fname" class="quantitat-boto" value="" placeholder="Màxim 4 unitats" maxlength="4">
+                        </form>
+                    </div>
+                </div>
+                <div class="producte3">
+                    <img class="imatgeProductes" src="./imatges/cafe2.jpg" alt="Cafe">
+                    <div class="titol"><h3>Cafè</h3></div>
+                    <div class="descripcio"><p>Cafè amb llet a escollir</p></div>
+                    <div class="preu"><h3>1,40 €</h3></div>
+                    <div>
+                        <form class="quantitat">
+                            <label for="fname">Quantitat:</label>
+                            <input type="number" name="fname" class="quantitat-boto" value="" placeholder="Màxim 4 unitats" maxlength="4">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="demanar">
+        <form action="" method="POST"><input class="demanar-boto" type="submit" value="Enviar comanda"></form>
+    </div>
+
+    <?php include "php/footer.php";?>
+    
+</body>
+</html>
