b316fdae2f67571c49529546667b79bcf4150de7
ShadowKiller11 <a19baylopmej@inspedralbes.cat>
2020-11-03 12:01:13 +0100

    canvis d'estructura

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 
 <?php
 
-
  $_SESSION['pantalla']=1; 
 
- 
- 
 ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,39 +17,30 @@
 
 </head>
 <body>
-    <?php
-        include 'php/header.php';
-    ?>
+    
+    <?php include 'php/header.php'; ?>
 
     <div class="caja_imagen img-fluid">
         <div class="texto_encima col-md-12">
             <h1>Haz Tu Pedido</h1>
         </div>
-        
         <div class="divbotons">
                <div id="menubutton">
                     <form action="phpfunctions/lan_menu.php">
-                        <input type="submit" value="Menú"></input>
+                        <input type="submit" value="Menú"/>
                     </form>
                 </div>
                 <div id="adminbutton">
-                <form action="phpfunctions/login.php">
-                        <input type="submit" value="Administració"></input>
-                </form>
+                    <form action="phpfunctions/login.php">
+                        <input type="submit" value="Administració"/>
+                    </form>
 
                 </div>
-            </div>
+        </div>
 
     </div>
     
-        
-           
-
-    <?php
-
-        include 'php/footer.php'
-
-    ?>
+    <?php include 'php/footer.php' ?>
   
 </body>
 </html>
