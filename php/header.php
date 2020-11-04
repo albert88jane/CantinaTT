11ea4f99e55793d579c58cc593e36fab743fac13
ShadowKiller11 <a19baylopmej@inspedralbes.cat>
2020-11-04 13:24:11 +0100

    mes canvis

--- php/header.php
+++ php/header.php
@@ -1,8 +1,7 @@
 <header>  
-    <div class="barranav"> 
-        
-        <a href="#index.php"><div class="logo"><img src="img/logo1.png" width="130px"></div></a>
-        <div class="titulo"><p>CANTINA PEDRALBES</p></div>
+        <div class="barranav"> 
+            <a href="http://localhost/CantinaTT/#index.php"><div class="logo"><img src="img/logo1.png" width="130px"></div></a>
+            <div class="titulo"><p>CANTINA PEDRALBES</p></div>
                
-    </div>
+        </div>
     </header> 
