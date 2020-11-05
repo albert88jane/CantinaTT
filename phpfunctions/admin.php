228c1664d4ec03ffc207b61e2352cef3e406fe45
albert88jane <73338253+albert88jane@users.noreply.github.com>
2020-11-05 14:27:07 +0100

    creació del gestor de fitchers php per a la pàgina d'admin

--- phpfunctions/admin.php
+++ phpfunctions/admin.php
@@ -1,9 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="../css/estilos.css">
+   <title>Document</title>
+</head>
+
+<body>
+
+
+<div class="container">
+
 <?php 
 
-   date_default_timezone_set('UTC');
-   $hoy = date('j / m / Y ');
-   echo $hoy;
+include '../php/header.php';
 
-   
+date_default_timezone_set('UTC');
+
+$hoy = date('j-m-Y ');
+
+echo $hoy;
+$hola = "Que tal?<br/>com estas?<br/><br/><br/><br/>";
+if(! file_exists("$hoy.txt")){
+
+$fitxer = fopen("../ficheros/$hoy.txt", "c+");
+
+
+}
+
+$fitxerdia = fopen("../ficheros/$hoy.txt","a");
+
+fwrite($fitxerdia, $hola . PHP_EOL);
+fclose($fitxerdia);
+
+
+$fitxerescriure = fopen("../ficheros/$hoy.txt","r");
+
+while(!feof($fitxerescriure)){
+
+   echo fgets($fitxerescriure) . "<br/>";
+}
+  
+
+  fclose($fitxerescriure);
+
+include '../php/footer.php';
 
 ?>
+
+</div>
+</body>
+</html>
+
+
