20ca66cf1477cd161443ecdbb69c6bf3c5d6edf4
AndreaPlatino <a19andronfue@inspedralbes.cat>
2020-11-05 02:12:18 +0100

    Afegeixo error.php + canvis en css + nova imatge per pantalla error

---
+++ error.php
@@ -0,0 +1,32 @@
+<?php  
+    session_start(); 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CANTINA PEDRALBES</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    <?php include './php/header.php';?> 
+    <div class="error">
+        <div class="missatge">
+            <div>
+                <img class="imatge-totem" src="./imatges/tassagominoles.jpg">
+            </div>
+            <div class="missatge-error">
+                <p>Ups! Sembla que ja has demanat.. Hauràs d'esperar a demà per fer una altra comanda.</p>
+            </div>
+        </div>
+        <div class="tornar">
+            <form id="tornar" action="" method="POST">
+                <input class="tornar-boto" type="submit" value="Torna">
+            </form>
+        </div>
+    </div>
+    <?php include "php/footer.php";?> 
+</body>
+</html>
