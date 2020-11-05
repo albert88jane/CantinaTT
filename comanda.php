3f3cf84a5a3269dc8da8cd160929771b645fc94d
AndreaPlatino <a19andronfue@inspedralbes.cat>
2020-11-05 13:13:55 +0100

    Afegeixo comanda.php + canvis css

---
+++ comanda.php
@@ -0,0 +1,33 @@
+<?php  
+    session_start(); 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CANTINA PEDRALBES</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    <?php include './php/header.php';?> 
+    <div class="pagina-comanda">
+        <div class="total-pagament">
+            <div class="llista-productes">
+            </div>
+            <hr class="separacio">
+            <div class="total">
+                <h3 id="titol-total" >TOTAL:</h3>
+                <h3 id="preu" >PREU €</h3>
+            </div>
+        </div>
+        <div class="confirmar-comanda">
+            <form id="confirmar" action="" method="POST">
+                <input class="confirmar-boto" type="submit" value="Confirmar comanda">
+            </form>
+        </div>
+    </div>
+    <?php include "php/footer.php";?> 
+</body>
+</html>
