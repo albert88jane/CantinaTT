c2264c632bf163d53b263a71e4e4aec48f0312de
albert88jane <73338253+albert88jane@users.noreply.github.com>
2020-11-05 22:37:41 +0100

    b

--- phpfunctions/formavanca.php
+++ phpfunctions/formavanca.php
@@ -3,9 +3,11 @@
     session_start();
 
     $_SESSION['pantalla']=3;
-    if(!isset ($_SESSION['DIA']) != date('j')){
-        
+    
+
+        $_SESSION['DIA'] = date('j'); 
+               
         header('Location:' . '../comanda.php');        
-    }
+    
 
 ?>
