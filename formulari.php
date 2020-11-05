cdeede37cb12a8c71ec3e5ea9c704105c8ad83d0
AlvaroelMan <a19sildur@inspedralbes.cat>
2020-11-06 00:11:34 +0100

    Cambios varios

--- formulari.php
+++ formulari.php
@@ -15,9 +15,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario</title>
     <link rel="stylesheet" href="css/estilos.css">
+    <link rel="javascript" href="Js/precio.js">
     
   
-      
+    <script>
+        document.write("Vivas el Betis");
+    let listaprod = JSON.parse(localStorage.getItem('producto'));
+    let listacant = JSON.parse(localStorage.getItem('cantidad'));
+    let productos = ["donut", "croissant", "canyes", "bikini", "entrepacalent", "pizza", "xocolata", "cafe", "suc", "sandwich", "entrepafred", "hamburguesa", "pastadeldia", "peixambverdures", "aigua", "gaseosa"];
+    let precio = [1.00, 1.20, 1.40, 1.60, 1.80, 1.40, 1.40, 1.20, 1.40, 2.00, 1.90, 3.90, 5.50, 6.80, 1.00, 1.20];
+    let total = [];
+
+    for (let i = 0; i < listaprod.length; i++) {
+        if (listaprod[i] == productos[i]) {
+            total[i] = listacant[i] * precio[i];
+        }
+    }
+    console.log(total)
+    </script>  
 </head>
 <body>
     
