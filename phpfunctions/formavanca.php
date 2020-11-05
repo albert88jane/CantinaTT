126bf03b9018a79b3bb7900db376173d3bb3aeff
albert88jane <73338253+albert88jane@users.noreply.github.com>
2020-11-05 22:55:39 +0100

    canvis fichers

--- phpfunctions/formavanca.php
+++ phpfunctions/formavanca.php
@@ -2,12 +2,35 @@
     
     session_start();
 
+
+    date_default_timezone_set('UTC');
+
+    $hoy = date('j-m-Y');
+
+    if(! file_exists("$hoy.txt")){
+
+        $fitxer = fopen("../ficheros/$hoy.txt", "c+");
+        fclose($fitxer);
+        
+        }
+        
+        $fitxerdia = fopen("../ficheros/$hoy.txt","a");
+        fwrite($fitxerdia, "<div class='comandaMostrar'>");
+
+        for ($i=0; $i < $comanda; $i++) { 
+            
+        
+            fwrite($fitxerdia, count($comanda) . PHP_EOL);
+
+        }
+        fwrite($fitxerdia, "</div>");
+        fclose($fitxerdia);
+
     $_SESSION['pantalla']=3;
     
+    $_SESSION['DIA'] = date('j'); 
 
-        $_SESSION['DIA'] = date('j'); 
-               
-        header('Location:' . '../comanda.php');        
+    header('Location:' . '../comanda.php');        
     
 
 ?>
