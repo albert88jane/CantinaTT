f4dfed42595a8db5f9f565b46afba7d23f2cfa34
AlvaroelMan <a19sildur@inspedralbes.cat>
2020-11-06 01:37:13 +0100

    Probando js

--- formulari.php
+++ formulari.php
@@ -15,24 +15,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario</title>
     <link rel="stylesheet" href="css/estilos.css">
-    <link rel="javascript" href="Js/precio.js">
+   
     
   
     <script>
-        document.write("Vivas el Betis");
-    let listaprod = JSON.parse(localStorage.getItem('producto'));
-    let listacant = JSON.parse(localStorage.getItem('cantidad'));
-    let productos = ["donut", "croissant", "canyes", "bikini", "entrepacalent", "pizza", "xocolata", "cafe", "suc", "sandwich", "entrepafred", "hamburguesa", "pastadeldia", "peixambverdures", "aigua", "gaseosa"];
-    let precio = [1.00, 1.20, 1.40, 1.60, 1.80, 1.40, 1.40, 1.20, 1.40, 2.00, 1.90, 3.90, 5.50, 6.80, 1.00, 1.20];
-    let total = [];
-
-    for (let i = 0; i < listaprod.length; i++) {
-        if (listaprod[i] == productos[i]) {
-            total[i] = listacant[i] * precio[i];
+        
+        let listaprod = JSON.parse(localStorage.getItem('producto'));
+        let listacant = JSON.parse(localStorage.getItem('cantidad'));
+        let productos = ["donut", "croissant", "canyes", "bikini", "entrepacalent", "pizza", "xocolata", "cafe", "suc", "sandwich", "entrepafred", "hamburguesa", "pastadeldia", "peixambverdures", "aigua", "gaseosa"];
+        let precio = [1.00, 1.20, 1.40, 1.60, 1.80, 1.40, 1.40, 1.20, 1.40, 2.00, 1.90, 3.90, 5.50, 6.80, 1.00, 1.20];
+        let listatotal = [];
+        let cont= 0;
+        for (let i = 0; i < listaprod.length; i++) {
+            if (listaprod[i] == productos[i]) {
+                
+                listatotal[cont] = listacant[i] * precio[i];
+                
+                cont++;
+            }
         }
-    }
-    console.log(total)
+        
+        console.log(cont);
+        console.log(listatotal);
+        let contenidoinput= "";
+        for (let i = 0; i < listaprod.length; i++) {
+            
+            contenidoinput += listaprod[i]+" x "+ listacant[i] + " = "+ listatotal[i] + "<br>"; 
+            
+        }
+        console.log(contenidoinput)
+        let input = document.getElementById("lista");
+        
+        
     </script>  
+    
+    
 </head>
 <body>
     
@@ -53,6 +70,7 @@
 	        <label for="email" >Correu Electrónic</label>
 	        <input type="email" name="email" id="email" placeholder="Email" required />
 	        <br>
+	        <input type="hidden" id="lista" required />
 	        
 	   </form>
 	       <div class="buttons">
