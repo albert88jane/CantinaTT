e2fbbaf58b2cf8fa51a56f03faa77b3044cc9b7c
ShadowKiller11 <a19baylopmej@inspedralbes.cat>
2020-11-06 12:13:42 +0100

    canvis en admin

---
+++ Admin/admin.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" href="../css/estilos.css">
+   <title>Document</title>
+</head>
+
+<body>
+
+
+    <div class="container">
+
+    <?php 
+
+        include '../php/header.php';
+
+        date_default_timezone_set('UTC');
+
+        $hoy = date('j-m-Y');
+
+
+        echo "<div class=fechaHoy>$hoy</div>";
+        $hola = "Que tal?<br/>com estas?<br/><br/><br/><br/>";
+
+        if(! file_exists("$hoy.txt")){
+
+        $fitxer = fopen("../ficheros/$hoy.txt", "c+");
+        fclose($fitxer);
+
+        }
+
+        $fitxerdia = fopen("../ficheros/$hoy.txt","a");
+        fwrite($fitxerdia, "<div class='comandaMostrar'>");
+        fwrite($fitxerdia, $hola . PHP_EOL);
+        fwrite($fitxerdia, "</div>");
+        fclose($fitxerdia);
+
+
+        $fitxerescriure = fopen("../ficheros/$hoy.txt","r");
+
+        while(!feof($fitxerescriure)){
+
+           echo fgets($fitxerescriure) . "<br/>";
+        }
+
+
+          fclose($fitxerescriure);
+
+
+    ?>
+
+   <div class="comandesAnteriors">
+
+
+      <div class='formulariAdmin'>
+
+
+          <h3> Escull un dia per a visitar les comandes Realitzades en aquest</h3>
+             <form action="diaEscollit.php" method="post">
+
+               <input type="text" name="diaE" id="diaE" placeholder="dd-mm-AAAA" />
+               <input type="submit" value="Buscar"  />
+
+            </form>
+      </div>
+
+
+
+
+   </div>
+
+</div>
+
+  <?php include '../php/footer.php'; ?>
+
+</body>
+
+</html>
+
+
